c786ff92b26d5af9e8835fa204aff047f6b0d502
DuyileS <duyile35@gmail.com>
2026-05-21 11:54:28 +0100

    10: widget session

--- apps/widget/next.config.mjs
+++ apps/widget/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  experimental: {
+    turbo: {
+      root: "../../",
+    },
+  },
 }
 
 export default nextConfig
