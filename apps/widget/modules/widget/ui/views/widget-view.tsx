226b99820c4b957cb73ca4eba2902a6858c470b9
DuyileS <duyile35@gmail.com>
2026-05-27 06:58:05 +0100

    13: conversations

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -7,6 +7,8 @@ import WidgetAuthScreen from "../screens/widget-auth-screen";
 import { screenAtom } from "../../atoms/widget-atoms";
 import WidgetErrorScreen from "../screens/widget-error-screen";
 import WidgetLoadingScreen from "../screens/widget-loading-screen";
+import WidgetSelectionScreen from "../screens/widget-selection-screen";
+import WidgetChatScreen from "../screens/widget-chat-screen";
 
 interface Props {
     organizationId: string | null;
@@ -19,11 +21,11 @@ const WidgetView = ({ organizationId }: Props) => {
     const screenComponents = {
         error: <WidgetErrorScreen />,
         loading: <WidgetLoadingScreen organizationId={organizationId} />,
-        selection: <p>TODO: Selection</p>,
+        selection: <WidgetSelectionScreen />,
         voice: <p>TODO: Voice</p>,
         auth: <WidgetAuthScreen />,
         inbox: <p>TODO: Inbox</p>,
-        chat: <p>TODO: Chat</p>,
+        chat: <WidgetChatScreen />,
         contact: <p>TODO: Contact</p>
     }
 
